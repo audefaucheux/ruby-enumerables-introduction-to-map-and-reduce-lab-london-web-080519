c7d3b8eb482ddc528a94ec8b76aadd6a53027f7c
Aude Faucheux <aude53@gmail.com>
2019-07-24 21:17:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,7 @@ def reduce_to_all_true(source_array)
   while i < source_array.length do
     if source_array[i] == true
       i += 1
-      return true
+      true
     else i += 1
     end
   end
