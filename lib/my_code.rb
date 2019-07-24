4b35f19369dd813e9eb4d1e6562e0de21c8d9ea0
Aude Faucheux <aude53@gmail.com>
2019-07-24 21:04:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,5 +15,11 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-
+  i = 0
+  new_array = []
+  while i < source_array.length do
+    element = source_array[i] * 2
+    new_array.push(element)
+    i += 1
+  end
 end
