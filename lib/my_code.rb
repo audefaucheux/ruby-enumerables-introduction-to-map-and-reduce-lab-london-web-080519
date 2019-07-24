55dba276fe2ad964c36d14b2877c3b333e5724d7
Aude Faucheux <aude53@gmail.com>
2019-07-24 21:07:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -24,3 +24,14 @@ def map_to_double(source_array)
   end
   return new_array
 end
+
+def map_to_square(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length do
+    element = source_array[i] ** 2
+    new_array.push(element)
+    i += 1
+  end
+  return new_array
+end
