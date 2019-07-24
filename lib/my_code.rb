96fb30bfe584dba066d34703a536fe448f8aa97e
Aude Faucheux <aude53@gmail.com>
2019-07-24 21:00:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
 # My Code here....
+def map_to_negativize(source_array)
+  i = 0
+  new_array = []
+  while source_array.length < i do
+    new_array.push(source_array[i])
+  end
+  return new_array
+end
